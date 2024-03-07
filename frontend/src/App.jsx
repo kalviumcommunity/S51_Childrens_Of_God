6f182e03bce00bf3bed125f6f0e21c866ec30833
devasree A <devasree.a@kalvium.community>
2024-03-07 14:46:29 +0530

    update and delete btn added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Dummy from './Components/Dummy'
 import OrphanageList from "./Components/fewdtobe"
 import Form from "./Components/Form"
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Update from './Components/Update'
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
       <Routes>
         <Route path='/' element={<OrphanageList />}></Route>
         <Route path='/Form' element={<Form />}></Route>
+        <Route path='/Update' element={<Update />}></Route>
       </Routes>
      
      </BrowserRouter>
