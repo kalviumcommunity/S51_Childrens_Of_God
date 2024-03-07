e0f27f4a56b0a7979f7f396afe39103ea256f301
devasree A <devasree.a@kalvium.community>
2024-03-07 18:09:41 +0530

    added patch and post method

--- Backend/Routes/Children.routes.js
+++ Backend/Routes/Children.routes.js
@@ -2,6 +2,8 @@ const express = require('express');
 const childrenRouter = express.Router();
 const ChildrenModel = require("../Children.model");
 
+const Joi = require("joi")
+
 childrenRouter.get('/getallchildren', async (req, res) => {
     try {
         const children = await ChildrenModel.find();
@@ -14,11 +16,31 @@ childrenRouter.get('/getallchildren', async (req, res) => {
         });
     }
 });
+const signupSchema = Joi.object({
+    ID:Joi.number().required(),
+    DONORNAME: Joi.string().required(),
+    INSTITUTIONNAME: Joi.string().required(),
+    AMOUNTGIVENBYDONOR: Joi.string().required(),
+});
 
 
 
 childrenRouter.post('/addchild', async (req, res) => {
+    const {error, value} = signupSchema.validate(req.body, {abortEarly:false});
+
+
+   
     try {
+        if (!error){
+            const {ID,DONORNAME,INSTITUTIONNAME,AMOUNTGIVENBYDONOR} = req.body
+            const newChild = await ChildrenModel.create({ID,DONORNAME,INSTITUTIONNAME,AMOUNTGIVENBYDONOR});
+            res.status(201).json(newChild);
+            }
+            else{
+                return res.status(400).send({
+                    message: `Bad request, error${error}`
+                })
+            }
         const child = await ChildrenModel.create(req.body);
         res.status(201).json(child);
     } catch (err) {
@@ -30,7 +52,9 @@ childrenRouter.post('/addchild', async (req, res) => {
 });
 
 childrenRouter.patch('/updateuser/:ID', async (req, res) => {
+    const {error, value} = signupSchema.validate(req.body, {abortEarly:false});
     try {
+        if (!error){
         const userId = req.params.ID;
         const updateFields = req.body;
 
@@ -47,6 +71,11 @@ childrenRouter.patch('/updateuser/:ID', async (req, res) => {
         );
 
         res.status(200).json(updatedUser);
+        }else{
+            return res.status(400).send({
+                message: `Bad request, error${error}`
+            })
+        }
     } catch (err) {
         console.error(err);
         return res.status(500).send({
@@ -58,8 +87,7 @@ childrenRouter.patch('/updateuser/:ID', async (req, res) => {
 
 childrenRouter.delete('/deleteuser/:ID', async (req, res) => {
     try {
-        const userId = req.params.ID;
-        const deletedUser = await ChildrenModel.findOneAndDelete({userId});
+        const deletedUser = await ChildrenModel.findOneAndDelete({ID:req.params.ID});
         res.status(200).json("deleted user");
     } catch (err) {
         console.error(err);
